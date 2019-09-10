fd60759ee05ab1471dcf463421f1c1ed33149320
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2019-09-09 22:25:07 -0400

    Add dummy security.h file and add to .gitignore

---
+++ security.h
@@ -0,0 +1,10 @@
+// NOTE: This is the dummy password file used for testing, not the actual password that will be used in the final flight software
+// Still, do NOT commit this file or any other security-related file to Git
+
+#ifndef SECURITY_H
+#define SECURITY_H
+
+// Must be 4 bytes
+#define SECURITY_CORRECT_PWD "UTAT"
+
+#endif
